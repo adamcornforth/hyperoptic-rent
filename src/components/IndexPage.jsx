82b8226bdd3f8af8ed274410bf199d39deea35e7
Adam Cornforth <adam@apcdesigner.com>
2016-11-27 14:04:17 +0000

    Add basic google maps component

--- src/components/IndexPage.jsx
+++ src/components/IndexPage.jsx
@@ -1,12 +1,20 @@
 // src/components/IndexPage.jsx
-import React from 'react';
+import React, {PropTypes, Component} from 'react';
+import GoogleMap from 'google-map-react';
 
 export default class IndexPage extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
-    this.state = { items: [] };
+    this.state = { 
+      items: [], 
+      gmap: {
+        center: {lat: 51.560782, lng: -0.120671},
+        zoom: 14
+      },
+    };
     this.componentDidMount = this.componentDidMount.bind(this);
   }
+
   componentDidMount() {
     fetch('/locations')
       .then((result) => {
@@ -20,10 +28,19 @@ export default class IndexPage extends React.Component {
     if(this.state.items.length) {
       return (
         <div>
+          <div className="map">
+            <GoogleMap
+              bootstrapURLKeys={{
+                key: 'AIzaSyCTbm6xphy2Y_qqpbj1NqqPZ0ZbcfXg7Qw'
+              }}
+              defaultCenter={this.state.gmap.center}
+              defaultZoom={this.state.gmap.zoom}>
+            </GoogleMap>
+          </div>
           {
             this.state.items.map((result, id) => {
               return (<div key={result.location.siteId}>
-                {result.location.siteName}
+                {result.location.siteName} - test
               </div>)
             })
           }
